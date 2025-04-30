f51125e252aa2743a7a32090348caa78e06db6fa
Hassan Farid <hassanbaba8142@gmail.com>
2025-04-30 13:36:36 +0500

    fixes

--- form-Validaiton/src/App.vue
+++ form-Validaiton/src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import TheWelcome from './components/TheWelcome.vue'
 import formValidation from './components/formValidation.vue'
 </script>
 
 <template>
   <div>
-    <!-- <TheWelcome /> -->
     <formValidation />
   </div>
   
